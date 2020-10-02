0e834bc3142a70b52027fcaa65c365b2c96ad668
kirill.fedyanin <kirill.fedyanin@gmail.com>
2020-10-02 15:38:30 +0300

    Initial commit

---
+++ some_func.py
@@ -0,0 +1,17 @@
+
+
+def main(message):
+    """
+    Some important notes about the function
+    It used as main
+    And do basicaly nothing
+    args:
+        message: just a string
+    return:
+        None
+    """
+    print(message)
+
+
+if __name__ == '__main__':
+    main("Docs are here")
